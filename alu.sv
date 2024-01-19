6276f831bfbeddc67a39fdbd7e7a356932c6df26
shyeonn <tkdgus2916@gmail.com>
2024-01-19 15:10:24 +0900

    ALU can calculate signed value

--- alu.sv
+++ alu.sv
@@ -14,8 +14,8 @@
 module alu
 #(  parameter REG_WIDTH = 32 )  // ALU input data width is equal to the width of register file
 (
-    input   [REG_WIDTH-1:0] in1,    // Operand 1
-    input   [REG_WIDTH-1:0] in2,    // Operand 2
+    input   signed [REG_WIDTH-1:0] in1,    // Operand 1
+    input   signed [REG_WIDTH-1:0] in2,    // Operand 2
     input   [3:0]   alu_control,    // ALU control signal
     output  logic [REG_WIDTH-1:0] result, // ALU output
     output          zero,           // Zero detection
@@ -26,9 +26,10 @@ module alu
         case (alu_control)
             4'b0000: result = in1 & in2;
             4'b0001: result = in1 | in2;
-            4'b0010: result = in1 + in2;
+            //4'b0010: result = in1 + in2;
 			4'b0011: result = in1 ^ in2;
             4'b0110: result = in1 - in2;
+			default: result = in1 + in2; 
 		endcase
     end
 
