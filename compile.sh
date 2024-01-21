0b964ffb902d032bf06c0a4424425ced7bf0e5a8
shyeonn <tkdgus2916@ajou.ac.kr>
2024-01-21 18:02:28 +0900

    Chanege compile script for unaligned memory

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-verilator -Wall --trace --cc --public single_cycle_cpu.sv imem.sv regfile.sv alu.sv dmem.sv --exe tb_single_cycle_cpu.cpp
+verilator -Wall --trace --cc --public single_cycle_cpu.sv imem.sv regfile.sv alu.sv dmem_unaligned.sv --exe tb_single_cycle_cpu.cpp
 make -C obj_dir -f Vsingle_cycle_cpu.mk Vsingle_cycle_cpu
 ./obj_dir/Vsingle_cycle_cpu
